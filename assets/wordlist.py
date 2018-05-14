e4683a7ea70a0f8f5083dcaed4d90746a3bfaf2f
wkirk01 <30280510+wkirk01@users.noreply.github.com>
2018-05-13 23:45:42 -0500

    Auto-generated commit

--- assets/wordlist.py
+++ assets/wordlist.py
@@ -13,3 +13,14 @@ with open('wordlist.json', 'w') as wf:
         wf.write("\t" + '"' + word.strip() + '"' + "," + '\n')
     wf.write('\n')
     wf.write("]")
+
+with open('wordlist_long.txt', 'r') as rf:
+    text_string = rf.readlines()
+
+with open('wordlist_long.json', 'w') as wf:
+    wf.write("[")
+    wf.write('\n')
+    for word in text_string:
+        wf.write("\t" + '"' + word.strip() + '"' + "," + '\n')
+    wf.write('\n')
+    wf.write("]")
